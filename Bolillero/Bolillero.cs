68032a87a53b96511dce922aba5a86f2f34caa14
Luis Duran <neoluis87@gmail.com>
2018-04-18 12:45:38 -0300

    Initial commit

---
+++ Bolillero/Bolillero.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPBolillero
+{
+    public class Bolillero: ICloneable
+    {
+        Random r;
+        public List<Byte> BolillasAdentro { get; private set; }
+
+        public List<Byte> BolillasFuera { get; private set; }
+
+        public Byte CantidadDeBolillas { get; private set; }
+
+        public Bolillero(byte cantidadDebolillas)
+        {
+            BolillasAdentro = new List<byte>();
+            BolillasFuera = new List<byte>();
+            crearBolillas(cantidadDebolillas);
+            r = new Random(DateTime.Now.Millisecond);
+        }
+
+        private Bolillero() 
+        {
+            r = new Random(DateTime.Now.Millisecond);
+        }
+
+        private void crearBolillas(byte cantidadBolillas)
+        {            
+            for (byte bolilla = 0; bolilla < cantidadBolillas; bolilla++)
+            {
+                BolillasAdentro.Add(bolilla);
+            }
+        }
+
+        public byte sacarBolillaAlAzar()
+        {
+            byte posicionBolilla = posicionDeBolillaAlAzar();
+            byte bolillaASacar = BolillasAdentro[posicionBolilla];
+            BolillasFuera.Add(bolillaASacar);
+            BolillasAdentro.RemoveAt(posicionBolilla);
+            return bolillaASacar;
+        }
+
+        public void volverAColocarBolillas()
+        {
+            BolillasAdentro.AddRange(BolillasFuera);
+            BolillasFuera.Clear();
+        }
+
+        private byte posicionDeBolillaAlAzar()
+        {
+            return (byte)r.Next(0, BolillasAdentro.Count);
+        }
+
+        public object Clone()
+        {
+            Bolillero clon = new Bolillero();
+            clon.CantidadDeBolillas = this.CantidadDeBolillas;
+            clon.BolillasAdentro = new List<byte>(this.BolillasAdentro);
+            clon.BolillasFuera = new List<byte>(this.BolillasFuera);
+            return clon;
+        }
+    }
+}
---
+++ PruebaBolillero/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TPBolillero;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PruebaBolillero
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        byte cantBolillas;
+        Bolillero bolillero;
+
+        [TestInitialize]
+        public void iniciar()
+        {
+            cantBolillas = 100;
+            bolillero = new Bolillero(cantBolillas);
+        }
+
+        [TestMethod]
+        public void cantidadDeBolillas()
+        {
+            Assert.AreEqual(cantBolillas, bolillero.BolillasAdentro.Count);
+            Assert.AreEqual(0, bolillero.BolillasFuera.Count);
+        }
+
+        [TestMethod]
+        public void efectoDelBolillero()
+        {
+            byte bolilla = bolillero.sacarBolillaAlAzar();
+            Assert.IsFalse(bolillero.BolillasAdentro.Contains(bolilla));
+            Assert.IsTrue(bolillero.BolillasFuera.Contains(bolilla));
+        }
+
+        [TestMethod]
+        public void probandoClon()
+        {
+            Bolillero otroBolillero = (Bolillero)bolillero.Clone();
+            Assert.AreEqual(otroBolillero.CantidadDeBolillas, bolillero.CantidadDeBolillas);
+            Assert.AreEqual(otroBolillero.BolillasAdentro.Count, bolillero.BolillasAdentro.Count);
+            Assert.IsTrue(otroBolillero.BolillasAdentro.TrueForAll(bolilla => bolillero.BolillasAdentro.Contains(bolilla)));
+        }
+
+    }
+}
