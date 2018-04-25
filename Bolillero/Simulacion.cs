1be1373fa01084bce5828d9e947c18f1bcdee65c
Luis Duran <neoluis87@gmail.com>
2018-04-25 13:59:58 -0300

    Agregado de clase Simulacion
    Tienen que desarrollar los metodos marcados con TODO vistos en clase.

---
+++ Bolillero/Simulacion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPBolillero
+{
+    public class Simulacion
+    {
+        public static bool acierta(Bolillero bolillero, List<Byte> lista)
+        {
+            return lista.All(numero => numero == bolillero.sacarBolillaAlAzar());            
+        }
+
+        public static ulong cantidadDeVecesQueGano(Bolillero bolillero, List<Byte> lista, ulong cantidadDeSimulaciones)
+        {
+            ulong cont = 0;
+            for (ulong i = 0; i < cantidadDeSimulaciones; i++, incrementarContadorSiEsNecesario(bolillero, lista, ref cont)) ;
+            return cont;
+        }
+
+        private static void incrementarContadorSiEsNecesario(Bolillero bolillero, List<Byte> lista, ref ulong cont)
+        {
+            //TODO
+        }
+
+        public static ulong cantidadDeVecesQueGano(Bolillero bolillero, List<Byte> lista, ulong cantidadDeSimulaciones, byte cantHilos)
+        {
+            //TODO
+        }
+
+        private static ulong sumarResultadoHilos(Task<ulong>[] hilos)
+        {
+            //TODO
+        }
+
+        private static void asignarTareas(Bolillero bolillero, Task<ulong>[] hilos, List<Byte> lista, ulong cantidadDeSimulaciones)
+        {
+            //
+        }
+
+    }
+}
--- PruebaBolillero/UnitTest1.cs
+++ PruebaBolillero/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using TPBolillero;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace PruebaBolillero
 {
@@ -9,12 +10,16 @@ namespace PruebaBolillero
     {
         byte cantBolillas;
         Bolillero bolillero;
+        ulong cantidadSimulaciones;
+        List<Byte> jugada;
 
         [TestInitialize]
         public void iniciar()
         {
-            cantBolillas = 100;
+            cantBolillas = 10;
             bolillero = new Bolillero(cantBolillas);
+            cantidadSimulaciones = 5000;
+            jugada = new List<byte> { 0 };            
         }
 
         [TestMethod]
@@ -40,6 +45,17 @@ namespace PruebaBolillero
             Assert.AreEqual(otroBolillero.BolillasAdentro.Count, bolillero.BolillasAdentro.Count);
             Assert.IsTrue(otroBolillero.BolillasAdentro.TrueForAll(bolilla => bolillero.BolillasAdentro.Contains(bolilla)));
         }
+        [TestMethod]
+        public void cantSimulacionesSinHilo()
+        {
+            ulong resultado = Simulacion.cantidadDeVecesQueGano(bolillero, jugada, cantidadSimulaciones);
+            Assert.AreEqual(cantidadSimulaciones/10 , resultado);
+        }
 
+        [TestMethod]
+        public void cantSimulacionesConHilo()
+        {
+            //TODO
+        }
     }
 }
